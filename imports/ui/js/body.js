ffd0a90736a6357fb7b453eb17e3a56ac5523f00
quent <quentin.philbert@gmail.com>
2017-04-20 23:08:24 +0200

    Fixing basics on radio because of facebook and youtubes API (2)

--- imports/ui/js/body.js
+++ imports/ui/js/body.js
@@ -4,18 +4,18 @@
 import { Template } from 'meteor/templating';
 
 import '../templates/body.html';
-import '../templates/chat.html';
+//import '../templates/chat.html';
 import '../templates/radio.html';
-import '/imports/ui/js/chat.js';
+//import '/imports/ui/js/chat.js';
 import '/imports/ui/js/radio.js';
 
 Template.facebooklogin.events({
     'click #facebook-login': function(event) {
-        Meteor.loginWithFacebook({requestPermissions: ['public_profile', 'email']}, function(err){
+        Meteor.loginWithFacebook({requestPermissions: ['user_friends', 'public_profile', 'email']}, function(err){
             if (err) {
                 console.log('Handle errors here: ', err);
             } else {
-                console.log(Meteor.user().services.facebook);
+                console.log(Meteor.user());
             }
         });
     },
